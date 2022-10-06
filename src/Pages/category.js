9a219f5c7306edf3b732229e88bb2fc525f2a30e
OpondoG <okonjogilbert2@gmail.com>
2022-10-06 16:19:03 +0300

    Redux store to display books

--- src/Pages/category.js
+++ src/Pages/category.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Category = () => (
-  <>
-    <h1>Category</h1>
-    <button type="button"> check status</button>
-  </>
-);
+const Category = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Category</h1>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(checkStatus());
+        }}
+      >
+        check status
+      </button>
+    </div>
+  );
+};
 
 export default Category;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,18 +5,20 @@ import Book from './Books';
 const BookList = (props) => {
   const { books } = props;
   return (
-    <ul>
-      {books.map(({ id, title, author }) => (
-        <Book key={id} title={title} author={author} />
-      ))}
-    </ul>
+    <>
+      <ul>
+        {books.map(({ id, title, author }) => (
+          <Book key={id} id={id} title={title} author={author} />
+        ))}
+      </ul>
+    </>
   );
 };
 
 BookList.propTypes = {
   books: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number,
+      id: PropTypes.string,
       title: PropTypes.string,
       author: PropTypes.string,
     }),
